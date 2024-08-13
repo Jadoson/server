ab3ca8ec77f2c849c834bc62bdea123ba5f2c336
Jadoson <ghost1342@gmail.com>
2024-08-13 19:51:20 +0300

    added index page and quiz page

---
+++ pages/quiz/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="my-8">
+    <h1 class="text-h3 mb-8">Choose quiz to edit</h1>
+    <v-card class="mx-auto" max-width="600">
+      <v-divider />
+      <v-card-actions>
+        <v-btn primary class="my-4 mx-4">✨ Create new Quiz</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
