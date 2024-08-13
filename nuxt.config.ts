958de03601d0be9ec70be7109db1244d5525fca1
Jadoson <ghost1342@gmail.com>
2024-08-13 19:26:20 +0300

    init app

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@invictus.codes/nuxt-vuetify'],
+  vuetify: {
+    moduleOptions: {
+      treeshaking: true,
+      useIconCDN: true,
+      styles: true,
+      autoImport: true,
+      useVuetifyLabs: true,
+    },
+  },
+})
